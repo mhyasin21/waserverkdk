46281afccae878bc9f2c8b48239601a1960261b2
mhyasin21 <98988042+mhyasin21@users.noreply.github.com>
2022-02-04 23:10:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -347,6 +347,6 @@ app.post('/clear-message', [
   })
 });
 
-server.listen(port, function() {
-  console.log('App running on *: ' + port);
+server.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
